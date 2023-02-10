c9169804a403a6a352c002fb3f0ecfd8418e4bc8
Jitendra Choudhary <jituchoudhary20@gmail.com>
2023-02-10 19:33:20 +0530

    data not visible

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,6 @@
  import React from 'react';
 
- export const Todo  = () =>{
+ export const Todo  = ({title,completed,id}) =>{
     return (
         <div className="todo" id={`todo-${id}`}>
             <div className="todo-text">{title}</div>
