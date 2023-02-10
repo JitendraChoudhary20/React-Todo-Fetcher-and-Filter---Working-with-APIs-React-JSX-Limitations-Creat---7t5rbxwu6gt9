46e430ab08eac934e9cac8b2968d0f4ee8f82284
JitendraChoudhary20 <112536233+JitendraChoudhary20@users.noreply.github.com>
2023-02-10 18:55:52 +0530

    copy

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,10 @@
  import React from 'react';
 
  export const Todo  = () =>{
-    
+    return (
+        <div className="todo" id={`todo-${id}`}>
+            <div className="todo-text">{title}</div>
+            <div className="todo-status">{completed ? 'Complete' : 'Incomplete'}</div>
+        </div>
+    )
  }
